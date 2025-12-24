6108611f01a04970d68c78de951f27939ee12a7e
devferbackend <devferbackend@gmail.com>
2025-12-24 13:04:49 -0600

    se crearon las variables globales humanScore y computerScore para contabilizar los juegos ganados

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//  let computerScore = 0
-//  let humanScore = 0
+//  
 //  let x = "rock"
 //  let y = "paper"
 //  let z = "scissors" 
@@ -11,6 +10,8 @@
 //  let messagelose2= "You lose, rock beats scissors!"
 //  let messagelose3= "You win, scissors beats paper!"
 //  let messagedraw1= "You tied!"
+let computerScore = 0
+ let humanScore = 0
 function getComputerChoice() {
 let com = Math.floor(Math.random() * 3) + 1;
 if (com===1) {
@@ -39,7 +40,6 @@ function playRound(humanChoice, computerChoice) {
  let human = humanChoice
  let computer = computerChoice
  let mensaje = ""
-
 if (human === computer) {
         mensaje = "This is a draw!";
     } else if (
@@ -47,18 +47,23 @@ if (human === computer) {
         (human === "paper" && computer === "rock") ||
         (human === "scissors" && computer === "paper")
     ) {
-        mensaje = `You win! ${human} beats ${computer}!`;
+        mensaje = `You win! ${human} beats ${computer}!`
+        humanScore = humanScore + 1
     } else {
-        mensaje = `You lose! ${computer} beats ${human}!`;
+        mensaje = `You lose! ${computer} beats ${human}!`
+        computerScore = computerScore + 1
     }
     
-    return mensaje;
+    return mensaje
+    
 }
 
    const humanSelection = getHumanChoice();
    const computerSelection = getComputerChoice();
-
+   
 
 console.log(`Human chose: ${humanSelection}`);
 console.log(`Computer chose: ${computerSelection}`);
 console.log(playRound(humanSelection, computerSelection));
+console.log(`Human: ${humanScore}`)
+console.log(`Computer: ${computerScore}`)
